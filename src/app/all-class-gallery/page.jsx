a2534499c53cc6c634af8e5fa008a3834134389c
Mr. Hasan <hasanshahriar32@gmail.com>
2023-09-08 01:08:21 +0600

    updated

--- src/app/all-class-gallery/page.jsx
+++ src/app/all-class-gallery/page.jsx
@@ -3,9 +3,12 @@ import React from "react";
 import AllStudentGallery from "../../Components/AllStudentGallery/AllStudentGallery";
 
 async function getData() {
-  const res = await fetch("https://bd-schools.vercel.app/api/add-gallery", {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    "https://bd-schools.vercel.app/api/add-gallery"
+    // {
+    //   cache: "no-store",
+    // }
+  );
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Failed To Fetch");
@@ -15,7 +18,11 @@ async function getData() {
 const AllClass = () => {
   const data = getData();
 
-  return <div>{/* <AllStudentGallery data={data}></AllStudentGallery> */}</div>;
+  return (
+    <div>
+      <AllStudentGallery data={data}></AllStudentGallery>
+    </div>
+  );
 };
 
 export default AllClass;
