6d76f7ae7bea5b3547ffffb10a3c11f8d027c960
Mr. Hasan <hasanshahriar32@gmail.com>
2023-09-08 01:00:04 +0600

    updated

--- src/app/(with-layout)/notice/page.jsx
+++ src/app/(with-layout)/notice/page.jsx
@@ -1,23 +1,31 @@
-"use client";
-import GetAllNotice from "../../../Components/GetAllNotice/GetAllNotice";
-async function getData() {
-  const res = await fetch("https://bd-schools.vercel.app/api/add-notice", {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error("Failed To Fetch");
-  }
-  return data;
-}
-const AllNoticePage = () => {
-  const data = getData();
-  console.log("all notice", data);
-  return (
-    <div>
-      <GetAllNotice></GetAllNotice>
-    </div>
-  );
+// "use client";
+// import GetAllNotice from "../../../Components/GetAllNotice/GetAllNotice";
+// async function getData() {
+//   const res = await fetch("https://bd-schools.vercel.app/api/add-notice", {
+//     cache: "no-store",
+//   });
+//   const data = await res.json();
+//   if (!res.ok) {
+//     throw new Error("Failed To Fetch");
+//   }
+//   return data;
+// }
+// const AllNoticePage = () => {
+//   const data = getData();
+//   console.log("all notice", data);
+//   return (
+//     <div>
+//       <GetAllNotice></GetAllNotice>
+//     </div>
+//   );
+// };
+
+// export default AllNoticePage;
+
+import React from "react";
+
+const page = () => {
+  return <div></div>;
 };
 
-export default AllNoticePage;
+export default page;
--- src/app/all-class-gallery/page.jsx
+++ src/app/all-class-gallery/page.jsx
@@ -1,25 +1,33 @@
-"use client";
+// "use client";
+// import React from "react";
+// import AllStudentGallery from "../../Components/AllStudentGallery/AllStudentGallery";
+
+// async function getData() {
+//   const res = await fetch("https://bd-schools.vercel.app/api/add-gallery", {
+//     cache: "no-store",
+//   });
+//   const data = await res.json();
+//   if (!res.ok) {
+//     throw new Error("Failed To Fetch");
+//   }
+//   return data;
+// }
+// const AllClass = () => {
+//   const data = getData();
+
+//   return (
+//     <div>
+//       <AllStudentGallery data={data}></AllStudentGallery>
+//     </div>
+//   );
+// };
+
+// export default AllClass;
+
 import React from "react";
-import AllStudentGallery from "../../Components/AllStudentGallery/AllStudentGallery";
 
-async function getData() {
-  const res = await fetch("https://bd-schools.vercel.app/api/add-gallery", {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error("Failed To Fetch");
-  }
-  return data;
-}
-const AllClass = () => {
-  const data = getData();
-
-  return (
-    <div>
-      <AllStudentGallery data={data}></AllStudentGallery>
-    </div>
-  );
+const page = () => {
+  return <div></div>;
 };
 
-export default AllClass;
+export default page;
